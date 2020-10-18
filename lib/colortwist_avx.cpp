808535697b3aa08c3d6ed5154cfcea951395f043
Juergen Bohl <jbohl@h-quer.de>
2020-10-18 17:19:31 +0200

    cosmetic

--- lib/colortwist_avx.cpp
+++ lib/colortwist_avx.cpp
@@ -48,34 +48,18 @@ bool colorTwistRGB48_AVX(const void* pSrc, uint32_t width, uint32_t height, int 
 
 bool colorTwistRGB48_AVX2(const void* pSrc, uint32_t width, uint32_t height, int strideSrc, void* pDst, int strideDst, const float* twistMatrix)
 {
-    /*__m128 t11t21t31 =  _mm256_broadcastsi128_si256(_mm_setr_ps(twistMatrix[0], twistMatrix[4], twistMatrix[8], 0));
-    __m128 t12t22t32 = _mm_setr_ps(twistMatrix[1], twistMatrix[5], twistMatrix[9], 0);
-    __m128 t13t23t33 = _mm_setr_ps(twistMatrix[2], twistMatrix[6], twistMatrix[10], 0);
-    __m128 t14t24t34 = _mm_setr_ps(twistMatrix[3], twistMatrix[7], twistMatrix[11], 0);*/
+    static const __m128i shuffleConst4 = _mm_setr_epi8(0, 1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 0, 0, 0, 0);
+    static const __m128i shuffleConst6 = _mm_setr_epi8(0, 1, 2, 3, 4, 5, -1, -1, 6, 7, 8, 9, 10, 11, -1, -1);
 
-    /*const __m256 t11t21t31 = _mm256_setr_ps(twistMatrix[0], twistMatrix[4], twistMatrix[8], 0, twistMatrix[0], twistMatrix[4], twistMatrix[8], 0);
-    const __m256 t12t22t32 = _mm256_setr_ps(twistMatrix[1], twistMatrix[5], twistMatrix[9], 0, twistMatrix[1], twistMatrix[5], twistMatrix[9], 0);
-    const __m256 t13t23t33 = _mm256_setr_ps(twistMatrix[2], twistMatrix[6], twistMatrix[10], 0, twistMatrix[2], twistMatrix[6], twistMatrix[10], 0);
-    const __m256 t14t24t34 = _mm256_setr_ps(twistMatrix[3], twistMatrix[7], twistMatrix[11], 0, twistMatrix[3], twistMatrix[7], twistMatrix[11], 0);*/
-    __m128 t11t12t13t14 = _mm_loadu_ps(twistMatrix + 0);// _mm_setr_ps(twistMatrix[0], twistMatrix[1], twistMatrix[2], twistMatrix[3]);
-    __m128 t21t212t23t24 = _mm_loadu_ps(twistMatrix + 4); //_mm_setr_ps(twistMatrix[4], twistMatrix[5], twistMatrix[6], twistMatrix[7]);
-    __m128 t31t32t33t34 = _mm_loadu_ps(twistMatrix + 8); //_mm_setr_ps(twistMatrix[8], twistMatrix[9], twistMatrix[10], twistMatrix[11]);
+    __m128 t11t12t13t14 = _mm_loadu_ps(twistMatrix + 0);
+    __m128 t21t22t23t24 = _mm_loadu_ps(twistMatrix + 4);
+    __m128 t31t32t33t34 = _mm_loadu_ps(twistMatrix + 8);
     __m128 t41t42t43t44 = _mm_setzero_ps();
-    _MM_TRANSPOSE4_PS(t11t12t13t14, t21t212t23t24, t31t32t33t34, t41t42t43t44);
+    _MM_TRANSPOSE4_PS(t11t12t13t14, t21t22t23t24, t31t32t33t34, t41t42t43t44);
     const __m256 t11t21t31 = _mm256_castsi256_ps(_mm256_broadcastsi128_si256(_mm_castps_si128(t11t12t13t14)));
-    const __m256 t12t22t32 = _mm256_castsi256_ps(_mm256_broadcastsi128_si256(_mm_castps_si128(t21t212t23t24)));
+    const __m256 t12t22t32 = _mm256_castsi256_ps(_mm256_broadcastsi128_si256(_mm_castps_si128(t21t22t23t24)));
     const __m256 t13t23t33 = _mm256_castsi256_ps(_mm256_broadcastsi128_si256(_mm_castps_si128(t31t32t33t34)));
     const __m256 t14t24t34 = _mm256_castsi256_ps(_mm256_broadcastsi128_si256(_mm_castps_si128(t41t42t43t44)));
-
-    static const __m256i shuffleConst1 = _mm256_setr_epi32(0, 0, 0, 0, 3, 3, 3, 3);
-    static const __m256i shuffleConst2 = _mm256_setr_epi32(1, 1, 1, 1, 4, 4, 4, 4);
-    static const __m256i shuffleConst3 = _mm256_setr_epi32(2, 2, 2, 2, 5, 5, 5, 5);
-
-    __m128i shuffleConst4 = _mm_setr_epi8(0, 1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 0, 0, 0, 0);
-
-    static const __m256i shuffleConst5 = _mm256_setr_epi32(0, 1, 2, 0xff, 3, 4, 5, 0xff);
-    //__m128i moveMask = _mm_setr_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0);
-    static const __m128i shuffleConst6 = _mm_setr_epi8(0, 1, 2, 3, 4, 5, -1, -1, 6, 7, 8, 9, 10, 11, -1, -1);
 
     for (size_t y = 0; y < height; ++y)
     {
@@ -83,43 +67,38 @@ bool colorTwistRGB48_AVX2(const void* pSrc, uint32_t width, uint32_t height, int
         uint16_t* d = (uint16_t*)(((uint8_t*)pDst) + y * strideDst);
         for (size_t x = 0; x < width / 4; ++x)
         {
-            __m128i src1 = _mm_loadu_si128((const __m128i*)p);
-            __m128i src1Shuffled = _mm_shuffle_epi8(src1, shuffleConst6);
-            __m256i src1Int32 = _mm256_cvtepu16_epi32(src1Shuffled);
-            __m256 src1Float = _mm256_cvtepi32_ps(src1Int32);
+            __m128i src1 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(p));     // load 8 words -> R1 G1 B1 R2 | G2 B2 R3 G3
+            __m128i src1Shuffled = _mm_shuffle_epi8(src1, shuffleConst6);            // shuffle to      R1 G1 B1 xx | R2 G2 B2 xx
+            __m256i src1Int32 = _mm256_cvtepu16_epi32(src1Shuffled);                 // convert to int32
+            __m256 src1Float = _mm256_cvtepi32_ps(src1Int32);                        // convert to float
 
-            __m128i src1r = _mm_bsrli_si128(src1, 12);
-            __m128i src2 = _mm_loadu_si64((const __m128i*)(p + 8));
-            __m128i src2_ = _mm_bslli_si128(src2, 4);
-            __m128i src2__ = _mm_or_si128(src1r, src2_);
-            __m128i src2Shuffled = _mm_shuffle_epi8(src2__, shuffleConst6);
-            __m256i src2Int32 = _mm256_cvtepu16_epi32(src2Shuffled);
-            __m256 src2Float = _mm256_cvtepi32_ps(src2Int32);
+            __m128i src1r3g3 = _mm_bsrli_si128(src1, 12);                            // shift so that we have R3 G3 00 00 | 00 00 00 00
+            __m128i src2 = _mm_loadu_si64((const __m128i*)(p + 8));                  // load 4 words -> B3 R4 G4 B4
+            __m128i src2Shifted = _mm_bslli_si128(src2, 4);                          // shift so that we have 00 00 B3 R4 | G4 B4 xx xx
+            __m128i src2Ored = _mm_or_si128(src1r3g3, src2Shifted);                  // or to get R3 G3 B3 R4 | G4 B4 xx xx
+            __m128i src2Shuffled = _mm_shuffle_epi8(src2Ored, shuffleConst6);        // shuffle to R3 G3 B3 xx | R4 G4 B4 xx
+            __m256i src2Int32 = _mm256_cvtepu16_epi32(src2Shuffled);                 // convert to int32
+            __m256 src2Float = _mm256_cvtepi32_ps(src2Int32);                        // convert to float
 
-            /*  __m256 r1r2 = _mm256_permutevar8x32_ps(src1Float, shuffleConst1);
-              __m256 g1g2 = _mm256_permutevar8x32_ps(src1Float, shuffleConst2);
-              __m256 b1b2 = _mm256_permutevar8x32_ps(src1Float, shuffleConst3);*/
-              //__m256 src1FloatShuffled = _mm256_permutevar8x32_ps(src1Float, shuffleConst5);
-            __m256 r1r2 = _mm256_shuffle_ps(src1Float, src1Float, 0);
-            __m256 g1g2 = _mm256_shuffle_ps(src1Float, src1Float, 0x55);
-            __m256 b1b2 = _mm256_shuffle_ps(src1Float, src1Float, 0xaa);
+            __m256 r1r2 = _mm256_shuffle_ps(src1Float, src1Float, 0);                // shuffle to get R1 R1 R1 xx | R2 R2 R2 xx
+            __m256 g1g2 = _mm256_shuffle_ps(src1Float, src1Float, 0x55);             // shuffle to get G1 G1 G1 xx | G2 G2 G2 xx
+            __m256 b1b2 = _mm256_shuffle_ps(src1Float, src1Float, 0xaa);             // shuffle to get B1 B1 B1 xx | B2 B2 B2 xx
 
+            __m256 result = _mm256_fmadd_ps(r1r2, t11t21t31, t14t24t34);             // we do the matrix-multiplication  R' = t11*R + t12*G + t13*B + t14
+            result = _mm256_fmadd_ps(g1g2, t12t22t32, result);                       //                                  G' = t21*R + t22*G + t23*B + t24
+            result = _mm256_fmadd_ps(b1b2, t13t23t33, result);                       //                                  B' = t31*R + t32*G + t33*B + t34
+                                                                                     // here, we now have R'1 G'1 B'1 xx | R'2 G'2 B'2 xx
 
-            __m256 result = _mm256_fmadd_ps(r1r2, t11t21t31, t14t24t34);
-            result = _mm256_fmadd_ps(g1g2, t12t22t32, result);
-            result = _mm256_fmadd_ps(b1b2, t13t23t33, result);
+            __m256i resultInteger = _mm256_cvtps_epi32(result);                      // convert to int32
+            __m256i resultUShorts = _mm256_packus_epi32(                             // convert to words    
+                resultInteger,
+                _mm256_castsi128_si256(_mm256_extracti128_si256(resultInteger, 1)));
 
-            __m256i resultInteger = _mm256_cvtps_epi32(result);
-            __m256i resultUShorts = _mm256_packus_epi32(resultInteger, /*_mm256_permute2f128_si256(resultInteger, resultInteger, 0x55)*/_mm256_castsi128_si256(_mm256_extracti128_si256(resultInteger, 1)));
-            __m128i resultUShorts2_1 = _mm_shuffle_epi8(_mm256_castsi256_si128(resultUShorts), shuffleConst4);
-            //_mm_maskmoveu_si128(resultUShorts2, moveMask, (char*)d);
+            __m128i resultUShortsShuffled = _mm_shuffle_epi8(                       // shuffle to get R'1 G'1 B'1 R'2 | G'2 B'2 xx xx (words)
+                _mm256_castsi256_si128(resultUShorts),
+                shuffleConst4);
 
-
-
-           /* r1r2 = _mm256_permutevar8x32_ps(src2Float, shuffleConst1);
-            g1g2 = _mm256_permutevar8x32_ps(src2Float, shuffleConst2);
-            b1b2 = _mm256_permutevar8x32_ps(src2Float, shuffleConst3);*/
-            // auto src1FloatShuffled = _mm256_permutevar8x32_ps(src2Float, shuffleConst5);
+            // now, the same for the third and fourth RGB-triple
             r1r2 = _mm256_shuffle_ps(src2Float, src2Float, 0);
             g1g2 = _mm256_shuffle_ps(src2Float, src2Float, 0x55);
             b1b2 = _mm256_shuffle_ps(src2Float, src2Float, 0xaa);
@@ -129,21 +108,17 @@ bool colorTwistRGB48_AVX2(const void* pSrc, uint32_t width, uint32_t height, int
             result = _mm256_fmadd_ps(b1b2, t13t23t33, result);
 
             resultInteger = _mm256_cvtps_epi32(result);
-            resultUShorts = _mm256_packus_epi32(resultInteger, /*_mm256_permute2f128_si256(resultInteger, resultInteger, 0x55)*/_mm256_castsi128_si256(_mm256_extracti128_si256(resultInteger, 1)));
-            __m128i resultUShorts2 = _mm_shuffle_epi8(_mm256_castsi256_si128(resultUShorts), shuffleConst4);
+            resultUShorts = _mm256_packus_epi32(resultInteger, _mm256_castsi128_si256(_mm256_extracti128_si256(resultInteger, 1)));
+            __m128i result2UShortsShuffled = _mm_shuffle_epi8(_mm256_castsi256_si128(resultUShorts), shuffleConst4);
 
-            /*int xx = _mm_extract_epi32(resultUShorts2, 0);
-            __m128i r = _mm_insert_epi32(resultUShorts2_1, xx, 3);*/
-
+            // we combine resultUShortsShuffled with result2UShortsShuffled to get: R'1 G'1 B'1 R'2 | G'2 B'2 R'3 G'3
             // it seems there is no "_m128i-intrinsic" corresponding to _mm_insert_ps, so we have a lot of casts here - however, those are "free"
-            __m128i r = _mm_castps_si128(_mm_insert_ps(_mm_castsi128_ps(resultUShorts2_1), _mm_castsi128_ps(resultUShorts2), 0x30));
+            __m128i r = _mm_castps_si128(_mm_insert_ps(_mm_castsi128_ps(resultUShortsShuffled), _mm_castsi128_ps(result2UShortsShuffled), 0x30));
+            _mm_storeu_si128(reinterpret_cast<__m128i*>(d), r);
 
-            _mm_storeu_si128((__m128i*)d, r);
-            __m128i r2 = _mm_bsrli_si128(resultUShorts2, 4);
-            _mm_storel_epi64((__m128i*)(d + 8), r2);
-
-            //_mm_maskmoveu_si128(resultUShorts2, moveMask, (char*)(d+6));
-
+            // finally, we now get B'3 R'4 G'4 B'4 and store it
+            __m128i r2 = _mm_bsrli_si128(result2UShortsShuffled, 4);
+            _mm_storel_epi64(reinterpret_cast<__m128i*>(d + 8), r2);
 
             p += 6 * 2;
             d += 6 * 2;
