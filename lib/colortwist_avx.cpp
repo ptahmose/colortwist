2d747306e602e4d8b0b179d9727d662396a88763
Juergen Bohl <jbohl@h-quer.de>
2020-10-19 23:31:35 +0200

    minor update

--- lib/colortwist_avx.cpp
+++ lib/colortwist_avx.cpp
@@ -215,20 +215,20 @@ bool colorTwistRGB48_AVX3(const void* pSrc, uint32_t width, uint32_t height, int
             __m256 resultB = _mm256_fmadd_ps(blueFloats, _mm256_set1_ps(t33), m2);
 
             __m256i resultInteger = _mm256_cvtps_epi32(resultR);                      // convert to int32, and then to words
-            __m256i resultRUShorts = _mm256_broadcastsi128_si256( _mm256_castsi256_si128(_mm256_packus_epi32(resultInteger,_mm256_castsi128_si256(_mm256_extracti128_si256(resultInteger, 1)))));
+            __m256i resultRUShorts = _mm256_broadcastsi128_si256(_mm_packus_epi32(_mm256_castsi256_si128(resultInteger), _mm256_extracti128_si256(resultInteger, 1)));
 
             resultInteger = _mm256_cvtps_epi32(resultG);
-            __m256i resultGUShorts = _mm256_broadcastsi128_si256(_mm256_castsi256_si128(_mm256_packus_epi32(resultInteger, _mm256_castsi128_si256(_mm256_extracti128_si256(resultInteger, 1)))));
+            __m256i resultGUShorts = _mm256_broadcastsi128_si256(_mm_packus_epi32(_mm256_castsi256_si128(resultInteger), _mm256_extracti128_si256(resultInteger, 1)));
 
             resultInteger = _mm256_cvtps_epi32(resultB);
-            __m256i resultBUShorts = _mm256_broadcastsi128_si256(_mm256_castsi256_si128(_mm256_packus_epi32(resultInteger, _mm256_castsi128_si256(_mm256_extracti128_si256(resultInteger, 1)))));
-            
+            __m256i resultBUShorts = _mm256_broadcastsi128_si256(_mm_packus_epi32(_mm256_castsi256_si128(resultInteger), _mm256_extracti128_si256(resultInteger, 1)));
+
             __m256i resultRShuffled1_256 = _mm256_shuffle_epi8(resultRUShorts, shuffleConst256_1);
             __m256i resultGShuffled2_256 = _mm256_shuffle_epi8(resultGUShorts, shuffleConst256_2);
             __m256i resultBShuffled3_256 = _mm256_shuffle_epi8(resultBUShorts, shuffleConst256_3);
             __m256i result256 = _mm256_or_si256(_mm256_or_si256(resultRShuffled1_256, resultGShuffled2_256), resultBShuffled3_256);
             _mm256_storeu_si256(reinterpret_cast<__m256i*>(d), result256);
-            
+
             __m128i resultRShuffled3 = _mm_shuffle_epi8(_mm256_castsi256_si128(resultRUShorts), shuffleConst10);
             __m128i resultGShuffled3 = _mm_shuffle_epi8(_mm256_castsi256_si128(resultGUShorts), shuffleConst11);
             __m128i resultBShuffled3 = _mm_shuffle_epi8(_mm256_castsi256_si128(resultBUShorts), shuffleConst12);
