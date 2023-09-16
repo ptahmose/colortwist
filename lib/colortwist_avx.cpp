285f68ac2f83c5845db6014954c2c76a84499a42
ptahmose <jbohl@h-quer.de>
2023-09-17 00:07:01 +0200

    test

--- lib/colortwist_avx.cpp
+++ lib/colortwist_avx.cpp
@@ -42,9 +42,12 @@ StatusCode colorTwistRGB48_AVX(const void* pSrc, uint32_t width, uint32_t height
             __m128i resultInteger = _mm_cvtps_epi32(result);
             __m128i resultShort = _mm_packus_epi32(resultInteger, resultInteger);
 
-            d[0] = resultShort.m128i_u16[0];
+          /*  d[0] = resultShort.m128i_u16[0];
             d[1] = resultShort.m128i_u16[1];
-            d[2] = resultShort.m128i_u16[2];
+            d[2] = resultShort.m128i_u16[2];*/
+            d[0] = static_cast<uint16_t>(_mm_extract_epi16(resultShort, 0));
+            d[1] = static_cast<uint16_t>(_mm_extract_epi16(resultShort, 1));
+            d[2] = static_cast<uint16_t>(_mm_extract_epi16(resultShort, 2));
             /*uint64_t _3words = _mm_cvtsi128_si64(resultShort);
             *reinterpret_cast<uint32_t*>(d) = static_cast<uint32_t>(_3words);
             *(d + 2) = static_cast<uint16_t>(_3words >> 32);*/
