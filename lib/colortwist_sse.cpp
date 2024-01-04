d0a67cf5fbd0ca1161e9c91d96a7f0735a2be991
ptahmose <jbohl@h-quer.de>
2024-01-05 00:58:35 +0100

    cosmetic

--- lib/colortwist_sse.cpp
+++ lib/colortwist_sse.cpp
@@ -10,6 +10,9 @@
 using namespace std;
 using namespace colortwist;
 
+// The three intrinsics _mm_loadu_si32, _mm_storeu_si32, _mm_loadu_si16 seem to be problematic with old versions 
+// of GCC (see e.g. https://stackoverflow.com/questions/72837929/mm-loadu-si32-not-recognized-by-gcc-on-ubuntu).
+// We try to work around this here.
 #if !COLORTWISTLIB_HAS_MM_LOADU_SI32_INTRINSICS
 inline __m128i mm_loadu_si32_workaround(const void* p)
 {
@@ -57,7 +60,7 @@ template <bool deal_with_remainder> struct ColorTwistRgb24Generic
 #if COLORTWISTLIB_HAS_MM_LOADU_SI32_INTRINSICS
                 __m128i b = _mm_loadu_si32(ps + 8);     // load 4 bytes  B2 R3 G3 B3
 #else
-                __m128i b = mm_loadu_si32_workaround(ps + 8);// _mm_castps_si128(_mm_load_ss(reinterpret_cast<const float*>(ps + 8)));
+                __m128i b = mm_loadu_si32_workaround(ps + 8);
 #endif
 
                 __m128i c = _mm_unpacklo_epi8(a, _mm_setzero_si128());  // convert from 8-bit to 16-bit (and 
@@ -120,7 +123,6 @@ template <bool deal_with_remainder> struct ColorTwistRgb24Generic
                 _mm_storeu_si32(pd + 8, result3_ui8);
 #else
                 mm_storeu_si32_workaround(pd + 8, result3_ui8);
-                //_mm_store_ss(reinterpret_cast<float*>(pd + 8), _mm_castsi128_ps(result3_ui8));
 #endif
 
                 pd += 12;
@@ -197,9 +199,9 @@ template <bool deal_with_remainder> struct ColorTwistRgb48Generic
                 __m128i first_rgb1_ushort16 = _mm_insert_epi16(first_rgbr_ushort16, 0x0001, 3);
 
 #if COLORTWISTLIB_HAS_MM_LOADU_SI32_INTRINSICS
-                __m128i c = _mm_loadu_si32(ps+8); // _mm_castps_si128(_mm_load_ss((const float*)(ps + 8)));
+                __m128i c = _mm_loadu_si32(ps + 8);
 #else
-                __m128i c = mm_loadu_si32_workaround(ps + 8); // _mm_castps_si128(_mm_load_ss((const float*)(ps + 8)));
+                __m128i c = mm_loadu_si32_workaround(ps + 8);
 #endif
 
                 __m128i second_rgb1_ushort16 = _mm_unpacklo_epi32(_mm_shuffle_epi32(first_rgbr_ushort16, 0x55), c);
@@ -229,10 +231,9 @@ template <bool deal_with_remainder> struct ColorTwistRgb48Generic
                 __m128i result_uint16_gb = _mm_packus_epi32(result_uint32_gb, result_uint32_gb);
 #if COLORTWISTLIB_HAS_MM_STOREU_SI32_INTRINSICS
                 _mm_storeu_si32(pd + 8, result_uint16_gb);
- #else
+#else
                 mm_storeu_si32_workaround(pd + 8, result_uint16_gb);
-                //_mm_store_ss((float*)(pd + 8), _mm_castsi128_ps(result_uint16_gb));
- #endif
+#endif
 
                 ps += 12;
                 pd += 12;
@@ -248,8 +249,7 @@ template <bool deal_with_remainder> struct ColorTwistRgb48Generic
 #if COLORTWISTLIB_HAS_MM_LOADU_SI16_INTRINSICS
                     __m128i b = _mm_loadu_si16(ps + 4); // load 2 bytes (the remainder of the SSE-register is zeroed)
 #else
-                    __m128i b = mm_loadu_si16_workaround(ps + 4); // _mm_castps_si128(_mm_load_ss(reinterpret_cast<const float*>(ps + 4)));
-                    //__m128i b = _mm_cvtsi32_si128((*reinterpret_cast<const uint16_t*>(ps + 4)));
+                    __m128i b = mm_loadu_si16_workaround(ps + 4);
 #endif
                     b = _mm_insert_epi16(b, 0x0001, 1);
 
